53b1a0bf3b0f05873f596c3b358ca570a6e331b0
Shraddha_Chhatraliya <skchhatraliya@gmail.com>
2021-12-07 18:39:08 +0530

    ''

---
+++ angular-13/src/app/dashboard/dashboard-components/top-selling/top-selling-data.ts
@@ -0,0 +1,45 @@
+export interface Product {
+    image: string,
+    uname: string,
+    gmail: string,
+    productName: string,
+    status: string,
+    budget: string
+}
+
+export const TopSelling: Product[] = [
+
+    {
+        image: 'assets/images/users/user1.jpg',
+        uname: 'Hanna Gover',
+        gmail: 'hgover@gmail.com',
+        productName: 'Flexy React',
+        status: 'danger',
+        budget: '95K'
+    },
+    {
+        image: 'assets/images/users/user2.jpg',
+        uname: 'Hanna Gover',
+        gmail: 'hgover@gmail.com',
+        productName: 'Landing pro React',
+        status: 'info',
+        budget: '95K'
+    },
+    {
+        image: 'assets/images/users/user3.jpg',
+        uname: 'Hanna Gover',
+        gmail: 'hgover@gmail.com',
+        productName: 'Elite React	',
+        status: 'warning',
+        budget: '95K'
+    },
+    {
+        image: 'assets/images/users/user4.jpg',
+        uname: 'Hanna Gover',
+        gmail: 'hgover@gmail.com',
+        productName: 'Ample React',
+        status: 'success',
+        budget: '95K'
+    },
+
+]
